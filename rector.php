1fc85746cdae8d8300297176ba6e63d50a7e4c0f
Gustavo Monteiro Favaro <gustavomonteirofavaro@gmail.com>
2025-01-05 09:34:47 -0300

    feat: add configuration rector

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/config',
+        __DIR__ . '/modules',
+        __DIR__ . '/public',
+        __DIR__ . '/tests',
+    ])
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets(
+        php84: true,
+    );
